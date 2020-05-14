4ff179189914ac1c123636cc4c61860502491b27
Avneet Kumar Sharma <avneet.sharma@noida.jkt.in>
2020-05-14 16:41:28 +0530

    add some java 8 logic

--- src/main/java/com/jkt/techbyte/menu/resource/MenuUtils.java
+++ src/main/java/com/jkt/techbyte/menu/resource/MenuUtils.java
@@ -7,18 +7,11 @@ import java.util.stream.Collectors;
 public class MenuUtils {
 
     public static UserResponce prepareUserMenus(List<RepositoryMenuModel> menuModelDaoList) {
-        UserMenuModel userMenuModel;
-        List<UserMenuModel> parentUserMenusList = new ArrayList<UserMenuModel>();
 
-        for (RepositoryMenuModel menuModelDao : menuModelDaoList) {
-            userMenuModel = new UserMenuModel();
-            if (menuModelDao.getMenuParentId() == 0) {
-                userMenuModel.setMenuId(menuModelDao.getMenuId());
-                userMenuModel.setMenuName(menuModelDao.getMenuName());
-                userMenuModel.setMenuParentId(menuModelDao.getMenuParentId());
-                parentUserMenusList.add(userMenuModel);
-            }
-        }
+        List<UserMenuModel> parentUserMenusList = menuModelDaoList.stream()
+                .filter(obj -> obj.getMenuParentId()==0)
+                .map(obj -> new UserMenuModel(obj))
+                .collect(Collectors.toList());
         List<UserMenuModel> userMenuList = addSubMenu(menuModelDaoList, parentUserMenusList);
         return new UserResponce(userMenuList);
     }
@@ -31,13 +24,9 @@ public class MenuUtils {
                 subMenu = new ArrayList<UserMenuModel>();
                 for (RepositoryMenuModel menuModelDao : menuModelDaoList) {
                     if (userMenuModel.getMenuId() == menuModelDao.getMenuParentId()) {
-                        UserMenuModel userMenuModelObj = new UserMenuModel();
-                        userMenuModelObj.setMenuId(menuModelDao.getMenuId());
-                        userMenuModelObj.setMenuName(menuModelDao.getMenuName());
-                        userMenuModelObj.setMenuParentId(menuModelDao.getMenuParentId());
+                        UserMenuModel userMenuModelObj = new UserMenuModel(menuModelDao);
                         subMenu.add(userMenuModelObj);
                     }
-
                 }
                 userMenuModel.setSubMenus(subMenu);
                 addSubMenu(menuModelDaoList, subMenu);
--- src/main/java/com/jkt/techbyte/menu/resource/UserMenuModel.java
+++ src/main/java/com/jkt/techbyte/menu/resource/UserMenuModel.java
@@ -1,15 +1,23 @@
 package com.jkt.techbyte.menu.resource;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserMenuModel {
 	private long menuId;
 	private String menuName;
 	private long menuParentId;
 	private List<UserMenuModel> subMenus;
+
+	public UserMenuModel(RepositoryMenuModel menuModelDao){
+		this.menuId= menuModelDao.getMenuId();
+		this.menuName= menuModelDao.getMenuName();
+		this.menuParentId= menuModelDao.getMenuParentId();
+	}
 }
